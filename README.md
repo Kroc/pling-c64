87a3da56fc72ec58d781719a66e0b813beb28f44
Kroc Camen <kroc@camendesign.com>
2020-01-22 18:23:07 +0000

    Build C64 1541 disk image
    PRG files don't auto-boot in VICE for some reason :/
    
    All X16 code has been removed. We'll come back to that once things have matured.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Pling!
 ================================================================================
-__Pling!__ is a native 8-bit Forth-like scripting language, under development, for 8-bit micro-computers; first for the Commodore 64 with plans for the Commander X16 later.
+__Pling!__ is a native 8-bit [Forth](https://en.wikipedia.org/wiki/Forth_(programming_language))-like scripting language, under development, for 8-bit micro-computers; first for the Commodore 64 with plans for the Commander X16 later.
 
 The term "Pling" is another name for an exclamantion mark, apparently coined by [Acorn Computers Ltd.](https://en.wikipedia.org/wiki/Acorn_Computers) as the term is used often in regards to [RISC OS](https://en.wikipedia.org/wiki/RISC_OS).
 
--- build.bat
+++ build.bat
@@ -4,11 +4,12 @@ CD %~dp0
 
 SET ACME=bin\acme\acme.exe -I "src"
 
-SET WLA_6510="bin\wla-dx\wla-6510.exe" -I "src"
-SET WLA_65C02="bin\wla-dx\wla-65c02.exe" -I "src"
-SET WLA_Z80="bin\wla-dx\wla-z80.exe" -I "src"
+SET WLA_6510="bin\wla-dx\wla-6510.exe" -i -I "src"
+SET WLA_65C02="bin\wla-dx\wla-65c02.exe" -i -I "src"
+SET WLA_Z80="bin\wla-dx\wla-z80.exe" -i -I "src"
 
-SET WLA_LINK="bin\wla-dx\wlalink.exe"
+SET WLA_LINK="bin\wla-dx\wlalink.exe" -i
+SET C1541="bin\vice\c1541.exe"
 
 SET X16EMU="bin\x16emu\x16emu.exe"
 SET VICE="bin\vice\x64.exe"
@@ -29,9 +30,16 @@ REM     -- "src\prg_pling_c64.acme"
 
 IF ERRORLEVEL 1 EXIT /B 1
 
-%WLA_LINK% -v ^
+%WLA_LINK% -v -S ^
     -b "link_c64.ini" ^
        "build\pling_c64.prg"
 
-REM %VICE% --autostart "%~dp0build\pling-c64.prg"
+IF ERRORLEVEL 1 EXIT /B 1
+
+REM # build a 1541 floppy disk image
+%C1541% ^
+    -format "pling!,00" d64 "build/pling-c64.d64" ^
+    -write  "build/pling_c64.prg" "pling!"
+
+%VICE% --autostart "%~dp0build\pling-c64.d64"
 REM %X16EMU% -debug -prg "%~dp0build\pling-x16.prg" -run
