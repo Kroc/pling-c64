ea4141d42202c0eee6e5d7861db2a3381524e4d3
Kroc Camen <kroc@camendesign.com>
2020-01-22 23:44:59 +0000

    Update to WLA-DX
    Compiled from latest source to use the C64 PRG file output.
    
    Removed ACME as no ACME code remains.

--- build.bat
+++ build.bat
@@ -2,27 +2,15 @@
 CLS & TITLE Building Pling!...
 CD %~dp0
 
-SET ACME=bin\acme\acme.exe -I "src"
-
 SET WLA_6510="bin\wla-dx\wla-6510.exe" -i -I "src"
 SET WLA_65C02="bin\wla-dx\wla-65c02.exe" -i -I "src"
 SET WLA_Z80="bin\wla-dx\wla-z80.exe" -i -I "src"
-
 SET WLA_LINK="bin\wla-dx\wlalink.exe" -i
+
 SET C1541="bin\vice\c1541.exe"
 
 SET X16EMU="bin\x16emu\x16emu.exe"
 SET VICE="bin\vice\x64.exe"
-
-REM %ACME% -v2 ^
-REM     --format cbm ^
-REM     --outfile "build\pling-x16.prg" ^
-REM     -- "src/prg_pling_x16.acme"
-
-REM %ACME% -v2 ^
-REM     --format    cbm ^
-REM     --outfile   "build\pling-c64.prg" ^
-REM     -- "src\prg_pling_c64.acme"
 
 %WLA_6510% -v ^
     -o "build\pling_c64.o" ^
@@ -30,7 +18,7 @@ REM     -- "src\prg_pling_c64.acme"
 
 IF ERRORLEVEL 1 EXIT /B 1
 
-%WLA_LINK% -v -S ^
+%WLA_LINK% -v -t c64PRG ^
     -b "link_c64.ini" ^
        "build\pling_c64.prg"
 
