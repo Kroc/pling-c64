66aaa5c1e21daf27fe41f06dd34edbbf52656bf1
Kroc Camen <kroc@camendesign.com>
2020-01-24 11:41:31 +0000

    Fix my completely broken stack logic
    That was a mess.

--- build.bat
+++ build.bat
@@ -4,7 +4,6 @@ CD %~dp0
 
 SET WLA_6510="bin\wla-dx\wla-6510.exe" -i -x -I "src"
 SET WLA_65C02="bin\wla-dx\wla-65c02.exe" -i -x -I "src"
-SET WLA_Z80="bin\wla-dx\wla-z80.exe" -i -x -I "src"
 SET WLA_LINK="bin\wla-dx\wlalink.exe" -i
 
 SET C1541="bin\vice\c1541.exe"
@@ -18,7 +17,7 @@ SET VICE="bin\vice\x64.exe"
 
 IF ERRORLEVEL 1 EXIT /B 1
 
-%WLA_LINK% -v -t c64PRG -S ^
+%WLA_LINK% -v -t CBMPRG -S ^
     -b "link_c64.ini" ^
        "build\pling_c64.prg"
 
@@ -27,7 +26,10 @@ IF ERRORLEVEL 1 EXIT /B 1
 REM # build a 1541 floppy disk image
 %C1541% ^
     -format "pling!,00" d64 "build/pling-c64.d64" ^
-    -write  "build/pling_c64.prg" "pling!"
+    -write  "build/pling_c64.prg" "pling!" ^
+    -write  "test.!" "test"
+
+IF ERRORLEVEL 1 EXIT /B 1
 
 %VICE% --autostart "%~dp0build\pling-c64.d64"
 REM %X16EMU% -debug -prg "%~dp0build\pling-x16.prg" -run
