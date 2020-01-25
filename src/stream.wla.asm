58f66f1cd7eaf115fd9b403c736dc7fe017c8b0b
Kroc Camen <kroc@camendesign.com>
2020-01-25 22:39:16 +0000

    Start working on interpreter
    Reads a term-name from an input buffer.

---
+++ src/stream.wla.asm
@@ -0,0 +1,17 @@
+; pling! (c) copyright Kroc Camen 2019-2020. unless otherwise noted,
+; licenced under a MIT licence; you may reuse and modify this code
+; how you please as long as you retain the copyright notice
+;
+; stream.wla : stream I/O
+;-------------------------------------------------------------------------------
+;
+;   <       read (generic)
+;   
+;           uses the stream handle atop the stack to read from the stream.
+;           the return value is dependent on the stream
+;
+;   >       write (generic)
+;
+;           uses the stream handle atop the stack to write to the stream
+;
+
